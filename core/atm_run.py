8646075ab05f17fbabbfc7c096e372b574bacc0d
hooby <hoobyhoobee@gmail.com>
2017-12-20 19:20:28 +0800

    s

--- core/atm_run.py
+++ core/atm_run.py
@@ -12,6 +12,9 @@ from core import terminal_op
 from core import terminal_repay,terminal_show_record,terminal_transfers,terminal_view_credit,\
     terminal_withdraw,terminal_discover_bills,terminal_bill_rate
 
+def logout():
+    exit('系统推出')
+
 menu = [terminal_view_credit.view_credit,
         terminal_show_record.show_record,
         terminal_repay.repay,
@@ -21,8 +24,6 @@ menu = [terminal_view_credit.view_credit,
         terminal_bill_rate.inqure_rates,
         logout]
 
-def logout():
-    exit('系统推出')
 
 
 def main():
